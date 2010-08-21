cbe8f17e94e0cb3c2f26f101a40a39f8cc5eff6e
Matt Burke <spraints@gmail.com>
2010-08-20 23:13:03 -0400

    Added twitter statuses to the paper.

--- public/app.js
+++ public/app.js
@@ -1,3 +1,21 @@
 $(function() {
   //$('.date').hover(... show menu
 });
+
+function showtweets(data) {
+  $(function() {
+    var twitter_story = document.createElement('div');
+    twitter_story.className = 'story twitter';
+    var number_of_results = data.results.length;
+    for(var i = 0; i < number_of_results; i++) {
+      var tweet = data.results[i];
+      var message = document.createElement('p');
+      var link = document.createElement('a');
+      link.href = 'http://twitter.com/' + tweet.from_user + "/status/" + tweet.id;
+      link.appendChild(document.createTextNode(tweet.text));
+      message.appendChild(link);
+      twitter_story.appendChild(message);
+    }
+    $(".right.column").append(twitter_story);
+  });
+}
