25c9f2c5d7a823841a650726348fe63058b56f3d
Matt Burke <spraints@gmail.com>
2010-09-03 08:24:57 -0400

    Added google analytics.

--- public/app.js
+++ public/app.js
@@ -19,3 +19,15 @@ function showtweets(data) {
     $(".right.column").append(twitter_story);
   });
 }
+
+// Google analytics:
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-18340745-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
