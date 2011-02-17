27fb851e3d071bae53b65937de8849bef21998af
Matt Burke <spraints@gmail.com>
2011-02-17 06:51:07 -0500

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pickardayune::Application.config.secret_token = 'f079e99e42a0a08318618e2ec7fe7f2a49c50d3e61e2bf7f367c2b11490f5c869c176b76d96e0fbe51bb2f4f225cff126f74b65f301b8ff7f1a57bd94fbe9b58'
