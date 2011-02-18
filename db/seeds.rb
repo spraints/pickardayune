e24f88c2e0a1a5043e8a159e3c81a17df546d1f3
Matt Burke <spraints@gmail.com>
2011-02-18 17:26:39 -0500

    Seed data (old stories).

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+puts "Creating an admin account for @spraints"
+Omnisocial::TwitterAccount.create! :remote_account_id => 14919968,
+  :user_id => User.create!(:role => 'admin')
+puts "Creating an admin account for @farmingengineer"
+Omnisocial::TwitterAccount.create! :remote_account_id => 27855975,
+  :user_id => User.create!(:role => 'admin')
+
+(Rails.root + 'db/seed/old_stories').children.select{|x| x.file?}.each do |file|
+  edition = file.basename('.haml').to_s
+  puts "Loading #{edition}..."
+  current_story = nil
+  current_column = nil
+  file.each_line do |line|
+    case line
+    when /@date\s*=/
+      #ignore
+    when /\.(.*)\.column/
+      current_column = $1
+      current_story.save! if current_story
+      current_story = nil
+    when /\.story/
+      current_story.save! if current_story
+      current_story = Story.new :edition => edition, :column => current_column
+    when /%h[34] (.*)/
+      current_story.title = $1.strip
+    when /%p (.*)/
+      (current_story.paragraphs ||= []) << $1.strip
+    else
+      puts "UNHANDLED: #{line.strip}"
+    end
+  end
+  current_story.save! if current_story
+end
