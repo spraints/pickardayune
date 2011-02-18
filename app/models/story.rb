33f833089a2fe0351b1eb34d3a17fdb1192c8913
Matt Burke <spraints@gmail.com>
2011-02-18 07:15:16 -0500

    Customize typus layout for Story.

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,9 +1,17 @@
 class Story < ActiveRecord::Base
   validates :title, :presence => true
-  COLUMN_CHOICES = %W(left middle right)
-  validates :column, :inclusion => COLUMN_CHOICES
+  COLUMN = %W(left middle right)
+  validates :column, :inclusion => COLUMN
   validates :edition, :format => /^\d\d\d\d-\d\d$/
   serialize :paragraphs, Array
 
   scope :for_edition, lambda { |date| where(:edition => date.strftime('%Y-%m')) }
+
+  def content_for_edit
+    (self.paragraphs || []).join("\n\n")
+  end
+
+  def content_for_edit=(content)
+    self.paragraphs = content && content.split(/\n\s*\n/)
+  end
 end
