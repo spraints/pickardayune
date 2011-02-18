4b7c3d9602760e5846d35755e6aee7f1c527e931
Matt Burke <spraints@gmail.com>
2011-02-18 14:18:26 -0500

    Keep API keys out of source control.

--- config/initializers/omnisocial.rb
+++ config/initializers/omnisocial.rb
@@ -9,11 +9,23 @@ Omnisocial.setup do |config|
   if Rails.env.production?
     
     # Configs for production mode go here
+    config.twitter ENV['TWITTER_APP_KEY'], ENV['TWITTER_APP_SECRET']
     
   elsif Rails.env.development?
     
-    # Configs for development mode go here
-    
+    begin
+      keys = YAML::load open(Pickardayune::Application.config.root + '.omnisocial.yml')
+      keys.each do |provider, keys|
+        config.send provider, *keys
+      end
+    rescue => e
+      puts "NO OMNISOCIAL CONFIGURATION HAS BEEN DONE!"
+      puts "Unable to load omnisocial config from .omnisocial.yml. Example:"
+      puts "  twitter: [ 'appkey', 'appsecret' ]"
+      puts "  facebook: [ 'appkey', 'appsecret', { :scope: publish_stream } ]"
+      puts e
+    end
+
   end
   
 end
